c31de335451ef1cbc1df3c245318137097d7c84c
Serggr <sergribtol@gmail.com>
2018-04-13 13:02:40 -0400

    Changes routing

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-/*
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-*/
-
 Route::resource('/','IndexController',['only'=>['index'],'names'=>['index'=>'home']]);
 Route::resource('portfolios','PortfolioController',[
     'parameters' => [
@@ -41,19 +21,13 @@ Route::resource('comment','CommentController',['only'=>['store']]);
 
 Route::match(['get','post'],'/contacts',['uses'=>'ContactsController@index','as'=>'contacts']);
 
-//Route::get('login',['uses' => 'Auth\LoginController@showLoginForm','as'=>'login']);
-//
-//Route::post('login','Auth\LoginController@login');
-//
-//Route::get('logout','Auth\LoginController@logout');
-
 Auth::routes();
+
 Route::get('logout', 'Auth\LoginController@logout');
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'],function(){
     
-    Route::get('/',['uses' => 'Admin\IndexController@index', 'as' => 'adminIndex']);
-    //Route::resource('articles','Admin\ArticlesController');
+    Route::get('/',['uses' => 'Admin\IndexController@index', 'as' => 'adminIndex']);   
     Route::resource('articles','Admin\ArticlesController', ['as' => 'admin']);
     Route::resource('permissions','Admin\PermissionsController', ['as' => 'admin']);
     Route::resource('menus','Admin\MenusController', ['as' => 'admin']);
